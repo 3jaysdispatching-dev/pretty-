e876a2a987d496549a2902219980a068dab4473f
3jaysdispatching-dev <3jaysdispatching@gmail.com>
2026-06-03 12:06:38 +0000

    ci: add GitHub Actions for pages, lint, link-check; add Storybook scaffold and package.json

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ['../Three-System/01_Design_System/Code_Libraries/React/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [],
+  framework: '@storybook/react'
+};
---
+++ Three-System/01_Design_System/Code_Libraries/React/stories/Button.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ButtonExample from '../ButtonExample';
+
+export default {
+  title: 'Components/Button',
+  component: ButtonExample,
+};
+
+export const Primary = () => <ButtonExample variant="primary">Primary</ButtonExample>;
